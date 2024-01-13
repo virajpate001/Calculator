36914a49690283e246738a73a9e39744e9e08227
virajpate001 <virajpate001@gmail.com>
2024-01-13 14:27:45 +0530

    calculator

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
+let resultArea = document.querySelector("#resultArea");
+
 function clearScreen(){
-    document.getElementById("fresult").value = "";
+  resultArea.value = "";
 }
 
 function display(value){
-    document.getElementById("fresult").value += value;
+  resultArea.value += value;
   }
 
-function clearonebyone(){
-    var inputElement= document.getElementById("fresult");
-    let inputText = inputElement.value;
-    let match = inputText.match(/\d$/);
-    if (match) {
-    let digitToRemove = match[0];
-    inputText = inputText.slice(0, -1); 
-  }
-  inputElement.value = inputText;
+function backspace(){
+   let currentValue = document.querySelector("#resultArea").value;
+   resultArea.value = currentValue. slice(0, -1);
 }
 
 function calculate(){
         
 
     try{
-      var resultval= document.getElementById("fresult").value;
+      var resultval= document.querySelector("#resultArea").value;
       var calvalue = eval(resultval);
-      document.getElementById("fresult").value = calvalue;
+      document.querySelector("#resultArea").value = calvalue;
 
     }
     catch (error){
-         console.error("An error occurred:", error.message);
+         
          alert("Enter Valid value");
-         document.getElementById("fresult").value = "";
+         document.querySelector("#resultArea").value = "";
     }
 }
